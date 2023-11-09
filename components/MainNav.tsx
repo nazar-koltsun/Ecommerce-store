caea6eebcaf9ff1fc8981e0e32a46e8da4b3da19
Nazar Koltsun <nazar-koltsun@ukr.net>
2023-11-09 16:21:01 +0200

    Added active state to nav links

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -1,18 +1,27 @@
+'use client';
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 const MainNav = () => {
+  const pageUrl = usePathname();
+
   const links = [
-    { href: '/products', label: 'Products' },
-    { href: '/about-us', label: 'About us' },
+    { href: '/products', label: 'Products', active: pageUrl === '/products' },
+    { href: '/about-us', label: 'About us', active: pageUrl === '/about-us' },
   ];
 
   return (
     <ul className="flex gap-4">
-      {links.map(({ href, label }) => (
+      {links.map(({ href, label, active }) => (
         <li key={`${href}${label}`}>
           <Link
             href={href}
-            className="text-[var(--link-color)] hover:text-gray-200"
+            className={cn(
+              "text-[var(--link-color)] hover:text-gray-200",
+              active && "text-gray-800",
+            )}
           >
             {label}
           </Link>

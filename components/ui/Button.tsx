1cecefeb0cf1511be60f9638db4974b0bc599922
Nazar Koltsun <nazar-koltsun@ukr.net>
2023-11-10 17:44:16 +0200

    New Button cmp

---
+++ components/ui/Button.tsx
@@ -0,0 +1,30 @@
+import { forwardRef } from 'react';
+import { cn } from '@/lib/utils';
+
+interface ButtonProps {
+  className?: string;
+  disabled?: boolean;
+  type?: 'button' | 'submit' | 'reset';
+  children?: React.ReactNode;
+  icon?: React.ReactNode;
+}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, disabled, type = 'button', children, ...props }, ref) => {
+    return (
+      <button
+        className={cn(
+          'w-auto, rounded-full bg-black border-transparent px-3 py-3 disabled:cursor-not-allowed disabled:opacity-50 disabled:bg-gray-400 disabled:text-gray-700 hover:bg-gray-700 hover:text-white transition-colors',
+          className
+        )}
+        ref={ref}
+        disabled={disabled}
+        type={type}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+
+export default Button;

3b7e229e7de5322e67ae24c9f2f24b01ee22744d
Nazar Koltsun <nazar-koltsun@ukr.net>
2023-11-13 16:17:51 +0200

    Minor fix

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,5 @@
 import Billboard from '@/components/Billboard';
-import getBillboard from "@/actions/get-billboards";
+import getBillboard from "@/actions/get-billboard";
 
 const Homepage = async () => {
   const billboard = await getBillboard('44099da6-98ca-4055-a0a8-d4695d4fc0d4');

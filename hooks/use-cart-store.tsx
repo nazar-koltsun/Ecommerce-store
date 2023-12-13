5cb9d4520a7b25b44470372f05fbcb5a780eb7be
Nazar Koltsun <nazar-koltsun@ukr.net>
2023-12-13 18:59:07 +0200

    Added useCartStore hook

---
+++ hooks/use-cart-store.tsx
@@ -0,0 +1,44 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+import toast from 'react-hot-toast';
+
+import { Product } from '@/types';
+
+interface CartStore {
+  items: Product[];
+  add: (product: Product) => void;
+  remove: (id: string) => void;
+  removeAll: () => void;
+}
+
+const useCartStore = create(
+  persist<CartStore>(
+    (set, get) => ({
+      items: [],
+      add: (product: Product) => {
+        const { items } = get();
+
+        if(items.find((item) => item.id === product.id)) {
+          toast.error('Already in cart');
+
+          return;
+        }
+
+        set({ items: [...items, product] });
+        toast.success('Added to cart');
+      },
+      remove: (id: string) => {
+        const { items } = get();
+        set({ items: items.filter((item) => item.id !== id) });
+        toast.success('Removed from cart');
+      },
+      removeAll: () => {
+        set({ items: [] });
+        toast.success('Removed all from cart');
+      } 
+    }),
+    { name: 'cart-storage', getStorage: () => localStorage }
+  )
+);
+
+export default useCartStore;
